29d0d3176cf1809856741e7126579e0ca73e2f0b
nagibator95 <43351648+nagibator95@users.noreply.github.com>
2019-05-21 22:27:47 +0300

    Some additions

--- listings/select_cache_meta.sql
+++ listings/select_cache_meta.sql
@@ -1,5 +1,5 @@
 SELECT key FROM
     monitor_cahce_meta
 WHERE
-    invalidate_args LIKE '%пользователь=1%'
-AND invalidate_args LIKE '%задача=1%'
+    invalidate_args LIKE '%polzovatel=1%'
+AND invalidate_args LIKE '%zada4a=1%'
